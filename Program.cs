0291a059aeca6fd105c4e097b098d8cefb4a583d
Visdroid <aldrinvisser@gmail.com>
2024-06-20 21:49:45 +0200

    1.3.Create React clientapp and link and initialize it Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using TicketTrackingSystem.Models;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>( options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+    builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+
+
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+
+
+//In production all the React Files will be served from here
+builder.Services.AddSpaStaticFiles(configuration = 
+{
+configuration.RootPath = "ClientApp/build";
+}>)
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseDatabaseErrorPage();
+}else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -15,13 +42,20 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseSpaStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
+app.UseEndPoints(endpoints =>
+{
+endpoints.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapRazoePages();
+});
+
 
 app.Run();
