be2e39fdfd34ec9e3a3380dccf4a69e0a96552a8
Visdroid <aldrinvisser@gmail.com>
2024-06-20 19:13:01 +0200

    1.Ticket System initalize

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TrackingTicketSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
